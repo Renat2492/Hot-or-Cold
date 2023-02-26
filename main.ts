f8551411f6c25da43875efa895320cff09ceffdc
Renat2492 <125185019+Renat2492@users.noreply.github.com>
2023-02-26 12:08:16 +0400

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+loops.everyInterval(500, function () {
+    if (input.temperature() > 24) {
+        basic.showString("Hot")
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else if (input.temperature() < 19) {
+        basic.showString("Cold")
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else {
+    	
+    }
+})
